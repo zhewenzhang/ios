9cbd7d21c38b223d48080b0699d7e4089568bfe1
zhewen <zhewen@zhewendeMacBook-Pro.local>
2017-03-01 00:18:01 +0800

    加入了登錄界面

--- myfirstApp/ViewController.swift
+++ myfirstApp/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func onClick(_ sender: Any) {
+        label.text = "hello,world"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
